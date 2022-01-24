d0a84d44e517311fadf2c53699f97df82ef5084f
Golamrabbani-ux <golamrabbani227417@gmail.com>
2022-01-24 11:20:10 +0600

    file upload heroku update

--- middleware/fileUpload.js
+++ middleware/fileUpload.js
@@ -12,7 +12,7 @@ const __dirname = path.resolve();
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, path.join(__dirname, "/uploads"));
+    cb(null, path.join(__dirname, "./uploads"));
   },
   filename: (req, file, cb) => {
     const extensionName = path.extname(file.originalname);
